16c3b97f9bacd9df1bf673a0a4b490cd8ad5347c
Wesley Haines <wesley.haines@me.com>
2015-01-17 10:32:39 -0800

    Initial Commit

---
+++ Tipper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipper
+//
+//  Created by Wesley Haines on 1/17/15.
+//  Copyright (c) 2015 Wesley Haines. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tipper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipper
+//
+//  Created by Wesley Haines on 1/17/15.
+//  Copyright (c) 2015 Wesley Haines. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
